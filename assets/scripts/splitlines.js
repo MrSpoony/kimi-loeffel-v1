e6ed5a129d67fdfe8fba0ed2937663b2dc9fd368
Kimi Löffel <kimi.loeffel@hispeed.ch>
2022-03-10 11:42:53 +0100

    Added impressum

---
+++ assets/scripts/splitlines.js
@@ -0,0 +1,2 @@
+let introduction = document.querySelector('.introduction p')
+introduction.innerHTML = "<span class='line'>"+(introduction.textContent.split("\n").filter(Boolean).join("</span>\n<span class='line'>"))+"</span>";
